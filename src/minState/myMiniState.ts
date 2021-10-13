44e91888f0532e0d2cf2ae44402743403d944796
lqd1434 <1434288209@qq.com>
2021-10-14 00:11:07 +0800

    fix:修复重用matadatakey可能会造成的问题,删除无用代码

--- src/minState/myMiniState.ts
+++ src/minState/myMiniState.ts
@@ -57,25 +57,24 @@ function create<T>(name:string,value:T):UStore<T>|null {
 	return store;
 }
 
-const ACTION_KEY = 'action'
 
 export function Action() {
 
 	return function (target,propKey,desc){
 		const originFunc = desc.value
+		const name = target.constructor.name
 		//修改
 		desc.value = function (...args){
-			const name = target.constructor.name
 			const result = originFunc.apply(this, args)
 			emitter.emit(name, this)
 			return result
 		}
 		//储存
-		const actions = Reflect.getMetadata(ACTION_KEY,target) as Array<string>
+		const actions = Reflect.getMetadata(`${name}:action`,target) as Array<string>
 		if (actions){
-				Reflect.defineMetadata(ACTION_KEY,[...actions,propKey],target)
+				Reflect.defineMetadata(`${name}:action`,[...actions,propKey],target)
 			} else {
-				Reflect.defineMetadata(ACTION_KEY,[propKey],target)
+				Reflect.defineMetadata(`${name}:action`,[propKey],target)
 			}
 		desc.enumerable = true
 		desc.configurable = true
@@ -88,30 +87,26 @@ export function State(initValue:any) {
 		let store = getStore(propKey);
 		const name = target.constructor.name
 		if (!store){
-			console.log('propKey',propKey)
 			 create(name,initValue)
 		}
-		Reflect.defineMetadata(name,propKey,target)
+		Reflect.defineMetadata(`${name}:state`,propKey,target)
 	}
 }
 
 export function useInjection<T extends Object>(Class:any ):T{
 	const className = (Class as Function).prototype.constructor.name
 	const [, setState] = useState({});
-	const stateKey = Reflect.getMetadata(className,Class.prototype) as string
+	const stateKey = Reflect.getMetadata(`${className}:state`,Class.prototype) as string
 	let store = getStore(className) as UStore<any>;
 	const res = {[stateKey]:store.state}
 	const instance = new Class() as T
-	const keys = Reflect.getMetadata(ACTION_KEY,Class.prototype) as Array<string>
+	const keys = Reflect.getMetadata(`${className}:action`,Class.prototype) as Array<string>
 
 	keys.forEach((key)=>{
 		Object.assign(res,{[key]:instance[key].bind(instance)})
 	})
-	console.log(Store)
-
 	useLayoutEffect(() => {
 		emitter.on<any>(className,(data)=>{
-			console.log(data)
 			store.dispatch(data[stateKey])
 			setState({})
 		})
